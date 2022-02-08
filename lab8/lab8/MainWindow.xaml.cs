371650cd47800d2462c5e200308580e1324e1f84
krystian-klosowicz <83827876+krystian-klosowicz@users.noreply.github.com>
2022-02-08 21:20:10 +0100

    Add files via upload

---
+++ lab8/lab8/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float x = float.Parse(box1.Text);
+                float y = float.Parse(box2.Text);
+
+
+                wynik.Text = "Suma: " + x + "+" + y + "=" + (x + y).ToString();
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Błędne dane!");
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float x = float.Parse(box1.Text);
+                float y = float.Parse(box2.Text);
+
+
+                wynik.Text = "Różnica: " + x + "-" + y + "=" + (x - y).ToString();
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Błędne dane!");
+            }
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float x = float.Parse(box1.Text);
+                float y = float.Parse(box2.Text);
+
+
+                wynik.Text = "Iloczyn: " + x + "*" + y + "=" + (x * y).ToString();
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Błędne dane!");
+            }
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float x = float.Parse(box1.Text);
+                float y = float.Parse(box2.Text);
+
+
+
+                wynik.Text = "Iloraz: " + x + "/" + y + "=" + (x / y).ToString();
+
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Dzielenie przez zero !");
+                wynik.Text = "Dzielenie przez zero !";
+            }
+
+        }
+    }
+}
